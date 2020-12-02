95c3c1daa21ebea37928179d6a16535cd5f76d09
Paulo Lopes <pmlopes@gmail.com>
2020-12-02 20:00:11 +0100

    Update howto
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- steps/step-2/src/main/java/vertx/HTTPSVerticle.java
+++ steps/step-2/src/main/java/vertx/HTTPSVerticle.java
@@ -1,15 +1,10 @@
 package vertx;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.net.JksOptions;
 
 public class HTTPSVerticle extends AbstractVerticle {
-
-  public static void main(String[] args) {
-    Vertx.vertx().deployVerticle(new HTTPSVerticle());
-  }
 
   @Override
   public void start() {
