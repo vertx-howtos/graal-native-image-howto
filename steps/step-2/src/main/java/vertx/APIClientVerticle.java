efb57c3399d8f64a165b8e16ef3d632563105bb5
Kevin S. Clarke <ksclarke@ksclarke.io>
2021-12-02 00:56:17 -0500

    Update native-image plugin (GraalVM 21.3.0 + Vert.x 4.2.1)

--- steps/step-2/src/main/java/vertx/APIClientVerticle.java
+++ steps/step-2/src/main/java/vertx/APIClientVerticle.java
@@ -1,3 +1,4 @@
+
 package vertx;
 
 import io.vertx.core.AbstractVerticle;
@@ -8,21 +9,22 @@ import io.vertx.ext.web.client.WebClientOptions;
 
 public class APIClientVerticle extends AbstractVerticle {
 
-  @Override
-  public void start() {
-    WebClient client = WebClient.create(vertx, new WebClientOptions().setSsl(true).setTrustAll(true));
+    @Override
+    public void start() {
+        final WebClient client = WebClient.create(vertx, new WebClientOptions().setSsl(true).setTrustAll(true));
 
-    client
-      .get(443, "icanhazdadjoke.com", "/")
-      .putHeader("Accept", "text/plain")
-      .send(ar -> {
-        if (ar.succeeded()) {
-          HttpResponse<Buffer> response = ar.result();
-          System.out.println("Got HTTP response with status " + response.statusCode() + " with data "
-              + response.body().toString("ISO-8859-1"));
-        } else {
-          ar.cause().printStackTrace();
-        }
-    });
-  }
+        client.get(443, "icanhazdadjoke.com", "/").putHeader("Accept", "text/plain").send(ar -> {
+            if (ar.succeeded()) {
+                final HttpResponse<Buffer> response = ar.result();
+
+                System.out.println("Got HTTP response with status " + response.statusCode() + " with data " +
+                        response.body().toString("ISO-8859-1"));
+            } else {
+                ar.cause().printStackTrace();
+            }
+
+            // Submit our API request and then exit (to make testing easier)
+            getVertx().close();
+        });
+    }
 }
