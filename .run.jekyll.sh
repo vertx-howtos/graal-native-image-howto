101c47e99e496c62fd42bd0d1bd9eba76bd56938
Thomas Segismont <tsegismo@redhat.com>
2024-12-05 14:59:27 +0100

    Use GH Action to deploy pages

--- .run.jekyll.sh
+++ .run.jekyll.sh
@@ -1,21 +1,9 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
 
 echo "⚙️  Building with Jekyll"
 gem install bundler
 bundle install
 bundle exec jekyll build
-
-echo "⚙️  Copying the files"
-rm -rf _gh_pages/*
-cp -R _site/* _gh_pages/
-
-echo "🚀 Commit and push"
-cd _gh_pages || exit
-git config --global user.email "howtos@vertx.io"
-git config --global user.name "Vert.x howtos"
-git add -A
-git commit -m "Deploy the how-to pages"
-git push origin gh-pages
 
 echo "✅ Done"
--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source "https://rubygems.org"
 
-gem "jekyll", "~> 3.8.5"
+gem "jekyll", "~> 3"
+gem "webrick"
 
 group :jekyll_plugins do
-  gem 'asciidoctor'
   gem 'jekyll-asciidoc'
 end
 
